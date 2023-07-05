e0e74b51d759382738524090900de31d035dbae1
Grace Holliday <ghollid@ncsu.edu>
2023-07-04 21:03:44 -0400

    Automation and misc. changes

---
+++ render_w_automation.R
@@ -0,0 +1,14 @@
+channels <- c("data_channel_is_lifestyle","data_channel_is_entertainment",
+    "data_channel_is_bus",
+    "data_channel_is_socmed",
+    "data_channel_is_tech",
+    "data_channel_is_world")
+
+for(i in 1:6)
+{rmarkdown::render("project2.rmd",
+                  output_format="html_document",
+                  params=list(datachannel=channels[i]),
+                  output_file = channels[i])
+}
+
+    
